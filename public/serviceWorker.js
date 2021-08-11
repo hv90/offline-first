1ba766ac3d2094104c78e87c9740efc904f8de18
hv90 <hugo.machado@hi.com.br>
2021-08-10 21:41:14 -0300

    initial commit

---
+++ public/serviceWorker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "v1";
+const urlsToCache = ["offline.html"];
+
+const self = this;
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Installed cache ");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Wait
+self.addEventListener("fetch", (event) => {
+  console.log("requested ", event.request);
+
+  event.respondWith(
+    caches.match("offline.html")
+    /* caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => {
+        console.log('catch');
+        caches.match('offline.html');
+      });
+    }), */
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
