2bb9a5da27b5be9164241ec2307a68aedc7ea447
hv90 <hugo.machado@hi.com.br>
2021-08-12 21:42:15 -0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,4 @@
 const CACHE_NAME = "v1";
-let a = 0;
-a++;
 const urlsToCache = ["offline.html", "logo512.png"];
 
 const self = this;
@@ -19,8 +17,19 @@ self.addEventListener("install", (event) => {
 // Wait
 self.addEventListener("fetch", (event) => {
   console.log("requested ", event.request);
+  console.log("The service worker is serving the asset.");
+  // You can use `respondWith()` to answer ASAP...
+  event.respondWith(fromCache(event.request));
+  // ...and `waitUntil()` to prevent the worker to be killed until
+  // the cache is updated.
+  event.waitUntil(
+    update(event.request)
+      // Finally, send a message to the client to inform it about the
+      // resource is up to date.
+      .then(refresh)
+  );
 
-  event.respondWith(
+  /*   event.respondWith(
     caches.match(event.request).then(() => {
       return fetch(event.request).catch(() =>
         caches.match("offline.html").catch((e) => {
@@ -28,7 +37,7 @@ self.addEventListener("fetch", (event) => {
         })
       );
     })
-  );
+  ); */
 });
 
 // Activate
@@ -51,3 +60,45 @@ self.addEventListener("activate", (event) => {
     )
   );
 });
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+// Sends a message to the clients.
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      // Encode which resource has been updated. By including the
+      // [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) the client can
+      // check if the content has changed.
+      var message = {
+        type: "refresh",
+        url: response.url,
+        // Notice not all servers return the ETag header. If this is not
+        // provided you should use other cache headers or rely on your own
+        // means to check if the content has changed.
+        eTag: response.headers.get("ETag"),
+      };
+      // Tell the client about the update.
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+}
